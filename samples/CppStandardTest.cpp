06e20cd7f17fab8fbbfa25dcf8a520450abd9b4f
高梓昆 <g1018505124@outlook.com>
2022-11-15 09:14:07 +0800

    fix CppAtandadTest.cpp bug

--- samples/CppStandardTest.cpp
+++ samples/CppStandardTest.cpp
@@ -1,16 +1,6 @@
-#include <concepts>
 #include <iostream>
 
-template<typename T, typename ... U>
-concept IsAnyOf = (std::same_as<T, U> || ...);
- 
-template<typename T>
-concept IsPrintable = std::integral<T> || std::floating_point<T> ||
-    IsAnyOf<std::remove_cvref_t<std::remove_pointer_t<std::decay_t<T>>>, char, wchar_t>;
- 
-void println(IsPrintable auto const ... arguments)
+int main()
 {
-    (std::wcout << ... << arguments) << '\n';
+    return 0;
 }
- 
-int main() { println("Example: ", 3.14, " : ", 42, " : [", 'a', L'-', L"Z]"); }
