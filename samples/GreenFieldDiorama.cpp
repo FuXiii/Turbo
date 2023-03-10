16cd29b4163db5b54cb8c141c426c34a9c5689a0
FuXii <g1018505124@outlook>
2023-03-10 11:01:31 +0800

    Update GreenFieldDiorama sample

--- samples/GreenFieldDiorama.cpp
+++ samples/GreenFieldDiorama.cpp
@@ -477,18 +477,8 @@ int main()
             ImGui::NewFrame();
 
             {
-                static float f = 0.0f;
-                static int counter = 0;
-
-                ImGui::Begin("Hello, world!"); // Create a window called "Hello, world!" and append into it.
-
-                ImGui::Text("This is some useful text."); // Display some text (you can use a format strings too)
-
-                if (ImGui::Button("Button")) // Buttons return true when clicked (most widgets return true when edited/activated)
-                    counter++;
-                ImGui::SameLine();
-                ImGui::Text("counter = %d", counter);
-
+                ImGui::Begin("GreenFieldDiorama");
+                ImGui::Text("shadertoy url: https://www.shadertoy.com/view/7dSGW1");
                 ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
                 ImGui::End();
             }
