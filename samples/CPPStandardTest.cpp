29a189aa921af90cdfdfcba3e09a128aa5558fed
画卦伏羲 <g1018505124@outlook.com>
2023-07-12 10:22:20 +0800

    remove SPIRV-Cross and VulkanMemoryAllocator

--- samples/CPPStandardTest.cpp
+++ samples/CPPStandardTest.cpp
@@ -1,16 +1,62 @@
-#include <concepts>
 #include <iostream>
+#include <list>
+#include <type_traits>
 
-template<typename T, typename ... U>
-concept IsAnyOf = (std::same_as<T, U> || ...);
- 
-template<typename T>
-concept IsPrintable = std::integral<T> || std::floating_point<T> ||
-    IsAnyOf<std::remove_cvref_t<std::remove_pointer_t<std::decay_t<T>>>, char, wchar_t>;
- 
-void println(IsPrintable auto const ... arguments)
+struct A_Test
 {
-    (std::wcout << ... << arguments) << '\n';
+};
+
+template <typename T>
+class TestUniformBuffer
+{
+  private:
+    uint32_t testValue;
+
+  public:
+    struct Descriptor
+    {
+        uint32_t testValue;
+    };
+
+    void Create(const std::string &name, const Descriptor &descriptor, void *allocator)
+    {
+        this->testValue = descriptor.testValue;
+        std::cout << "Size:" << sizeof(T) << std::endl;
+    }
+
+    uint32_t GetValue() const
+    {
+        return this->testValue;
+    }
+};
+
+class TestContext
+{
+  public:
+    template <typename T>
+    void BindingTestUniformBuffer(const TestUniformBuffer<T> &tub)
+    {
+        std::cout << "Value:" << tub.GetValue() << std::endl;
+    }
+};
+
+int main()
+{
+    std::cout << "Hello World" << std::endl;
+
+    bool is_class = std::is_class_v<A_Test>;
+    std::cout << is_class << ":A_Test" << std::endl;
+
+    struct B_Test
+    {
+        float test;
+        uint16_t uint16_t_test;
+        double double_test;
+    };
+
+    TestUniformBuffer<B_Test> test_buffer;
+    test_buffer.Create("test uniform bffer", {123}, nullptr);
+
+    TestContext tc;
+    tc.BindingTestUniformBuffer(test_buffer);
 }
- 
-int main() { println("Example: ", 3.14, " : ", 42, " : [", 'a', L'-', L"Z]"); }
