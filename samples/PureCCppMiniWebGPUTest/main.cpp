4f609332dcd4a8a7dcee22c77a1a5ca115d1949d
画卦伏羲 <g1018505124@outlook.com>
2023-08-01 17:32:14 +0800

    Update samples\PureCCppMiniWebGPUTest\main.cpp for c

--- samples/PureCCppMiniWebGPUTest/main.cpp
+++ samples/PureCCppMiniWebGPUTest/main.cpp
@@ -1,6 +1,7 @@
 #include <GLFW/glfw3.h>
 #include <emscripten/emscripten.h>
 #include <emscripten/html5.h>
+#include <emscripten/html5_webgpu.h>
 #include <iostream>
 
 #include <webgpu/webgpu.h>
@@ -35,80 +36,6 @@ void GetDevice(void (*callback)(wgpu::Device))
                 exit(0);
             }
             wgpu::Adapter adapter = wgpu::Adapter::Acquire(cAdapter);
-
-            // {
-            //     wgpu::AdapterProperties adapter_properties = {};
-            //     adapter.GetProperties(&adapter_properties);
-
-            //     std::cout << "vendorID::" << adapter_properties.vendorID << std::endl;
-            //     std::cout << "vendorName::" << adapter_properties.vendorName << std::endl;
-            //     std::cout << "architecture::" << adapter_properties.architecture << std::endl;
-            //     std::cout << "deviceID::" << adapter_properties.deviceID << std::endl;
-            //     std::cout << "name::" << adapter_properties.name << std::endl;
-            //     std::cout << "driverDescription::" << adapter_properties.driverDescription << std::endl;
-
-            //     switch (adapter_properties.adapterType)
-            //     {
-            //     case wgpu::AdapterType::DiscreteGPU: {
-            //         std::cout << "adapterType::DiscreteGPU" << std::endl;
-            //     }
-            //     break;
-            //     case wgpu::AdapterType::IntegratedGPU: {
-            //         std::cout << "adapterType::IntegratedGPU" << std::endl;
-            //     }
-            //     break;
-            //     case wgpu::AdapterType::CPU: {
-            //         std::cout << "adapterType::CPU" << std::endl;
-            //     }
-            //     break;
-            //     case wgpu::AdapterType::Unknown: {
-            //         std::cout << "adapterType::Unknown" << std::endl;
-            //     }
-            //     break;
-            //     }
-
-            //     switch (adapter_properties.backendType)
-            //     {
-            //     case wgpu::BackendType::Undefined: {
-            //         std::cout << "backendType::Undefined" << std::endl;
-            //     }
-            //     break;
-            //     case wgpu::BackendType::Null: {
-            //         std::cout << "backendType::Null" << std::endl;
-            //     }
-            //     break;
-            //     case wgpu::BackendType::WebGPU: {
-            //         std::cout << "backendType::WebGPU" << std::endl;
-            //     }
-            //     break;
-            //     case wgpu::BackendType::D3D11: {
-            //         std::cout << "backendType::D3D11" << std::endl;
-            //     }
-            //     break;
-            //     case wgpu::BackendType::D3D12: {
-            //         std::cout << "backendType::D3D12" << std::endl;
-            //     }
-            //     break;
-            //     case wgpu::BackendType::Metal: {
-            //         std::cout << "backendType::Metal" << std::endl;
-            //     }
-            //     break;
-            //     case wgpu::BackendType::Vulkan: {
-            //         std::cout << "backendType::Vulkan" << std::endl;
-            //     }
-            //     break;
-            //     case wgpu::BackendType::OpenGL: {
-            //         std::cout << "backendType::OpenGL" << std::endl;
-            //     }
-            //     break;
-            //     case wgpu::BackendType::OpenGLES: {
-            //         std::cout << "backendType::OpenGLES" << std::endl;
-            //     }
-            //     break;
-            //     }
-            //     std::cout << "compatibilityMode::" << adapter_properties.compatibilityMode << std::endl;
-            // }
-
             adapter.RequestDevice(
                 nullptr,
                 [](WGPURequestDeviceStatus status, WGPUDevice cDevice, const char *message, void *userdata) {
@@ -122,11 +49,7 @@ void GetDevice(void (*callback)(wgpu::Device))
 
 void SetupSwapChain(wgpu::Surface surface)
 {
-    wgpu::SwapChainDescriptor scDesc{.usage = wgpu::TextureUsage::RenderAttachment,
-                                     .format = wgpu::TextureFormat::BGRA8Unorm,
-                                     .width = kWidth,
-                                     .height = kHeight,
-                                     .presentMode = wgpu::PresentMode::Fifo};
+    wgpu::SwapChainDescriptor scDesc{.usage = wgpu::TextureUsage::RenderAttachment, .format = wgpu::TextureFormat::BGRA8Unorm, .width = kWidth, .height = kHeight, .presentMode = wgpu::PresentMode::Fifo};
     SWAP_CHAIN = DEVICE.CreateSwapChain(surface, &scDesc);
 }
 
@@ -140,11 +63,9 @@ void CreateRenderPipeline()
 
     wgpu::ColorTargetState colorTargetState{.format = wgpu::TextureFormat::BGRA8Unorm};
 
-    wgpu::FragmentState fragmentState{
-        .module = shaderModule, .entryPoint = "fragmentMain", .targetCount = 1, .targets = &colorTargetState};
+    wgpu::FragmentState fragmentState{.module = shaderModule, .entryPoint = "fragmentMain", .targetCount = 1, .targets = &colorTargetState};
 
-    wgpu::RenderPipelineDescriptor descriptor{.vertex = {.module = shaderModule, .entryPoint = "vertexMain"},
-                                              .fragment = &fragmentState};
+    wgpu::RenderPipelineDescriptor descriptor{.vertex = {.module = shaderModule, .entryPoint = "vertexMain"}, .fragment = &fragmentState};
     PIPELINE = DEVICE.CreateRenderPipeline(&descriptor);
 }
 
@@ -159,10 +80,7 @@ void Render()
     double time = glfwGetTime();
     double coefficient = (sin(time) + 1) * 0.5;
     wgpu::Color clear_color = {.r = coefficient, .g = coefficient, .b = coefficient, .a = 1};
-    wgpu::RenderPassColorAttachment attachment{.view = SWAP_CHAIN.GetCurrentTextureView(),
-                                               .loadOp = wgpu::LoadOp::Clear,
-                                               .storeOp = wgpu::StoreOp::Store,
-                                               .clearValue = clear_color};
+    wgpu::RenderPassColorAttachment attachment{.view = SWAP_CHAIN.GetCurrentTextureView(), .loadOp = wgpu::LoadOp::Clear, .storeOp = wgpu::StoreOp::Store, .clearValue = clear_color};
 
     wgpu::RenderPassDescriptor renderpass{.colorAttachmentCount = 1, .colorAttachments = &attachment};
 
@@ -207,11 +125,95 @@ void Start()
     emscripten_request_animation_frame_loop(EMSCRIPTEN_ANIMATION_FRAME, window);
 }
 
+#include <future>
+#include <thread>
+
+int Test()
+{
+    WGPUInstanceDescriptor wgpu_instance_descriptor = {};
+    wgpu_instance_descriptor.nextInChain = nullptr;
+    WGPUInstance wgpu_instance = wgpuCreateInstance(&wgpu_instance_descriptor);
+
+    if (wgpu_instance != nullptr)
+    {
+        std::cout << "WGPUInstance create success" << std::endl;
+    }
+    else
+    {
+        std::cout << "WGPUInstance create failed" << std::endl;
+        return -1;
+    }
+
+    WGPUAdapter wgpu_adapter = nullptr;
+
+    //std::future<WGPUAdapter> f2 = std::async(std::launch::async, [] { WGPUAdapter return_wgpu_adapter = nullptr });
+
+    WGPURequestAdapterCallback wgpu_request_adapter_callback = [](WGPURequestAdapterStatus status, WGPUAdapter adapter, char const *message, void *userdata) {
+        switch (status)
+        {
+        case WGPURequestAdapterStatus::WGPURequestAdapterStatus_Success: {
+            WGPUAdapter *temp_wgpu_adapter = reinterpret_cast<WGPUAdapter *>(userdata);
+            *temp_wgpu_adapter = adapter;
+            std::cout << "WGPURequestAdapterCallback Success" << std::endl;
+
+            if (adapter != nullptr)
+            {
+                std::cout << "adapter Success" << std::endl;
+            }
+            else
+            {
+                std::cout << "adapter nullptr" << std::endl;
+            }
+        }
+        break;
+        case WGPURequestAdapterStatus::WGPURequestAdapterStatus_Unavailable:
+        case WGPURequestAdapterStatus::WGPURequestAdapterStatus_Error:
+        case WGPURequestAdapterStatus::WGPURequestAdapterStatus_Unknown: {
+            throw std::runtime_error(std::string("Unsupport WebGPU"));
+        }
+        break;
+        }
+    };
+
+    WGPURequestAdapterOptions wgpu_request_adapter_options = {};
+    wgpu_request_adapter_options.nextInChain = nullptr;
+    wgpu_request_adapter_options.compatibleSurface = nullptr;
+    wgpu_request_adapter_options.powerPreference = WGPUPowerPreference::WGPUPowerPreference_LowPower;
+    wgpu_request_adapter_options.backendType = WGPUBackendType::WGPUBackendType_WebGPU;
+    wgpu_request_adapter_options.forceFallbackAdapter = false;
+    wgpu_request_adapter_options.compatibilityMode = false;
+
+    wgpuInstanceRequestAdapter(wgpu_instance, &wgpu_request_adapter_options, wgpu_request_adapter_callback, (void *)&wgpu_adapter);
+
+    // while (wgpu_adapter == nullptr)
+    // {
+    //     std::cout << "waite for wgpu_adapter" << std::endl;
+    // }
+
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+
+    if (wgpu_adapter != nullptr)
+    {
+        std::cout << "wgpu_adapter Success" << std::endl;
+    }
+    else
+    {
+        std::cout << "wgpu_adapter nullptr" << std::endl;
+    }
+    wgpuInstanceRelease(wgpu_instance);
+    return 0;
+}
+
 int main()
 {
+    Test();
+
     INSTANCE = wgpu::CreateInstance();
+
     GetDevice([](wgpu::Device device) {
         DEVICE = device;
         Start();
     });
+
+    return 0;
 }
